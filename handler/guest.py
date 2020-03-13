6d39abba5a868e5f5f0fd1356b3cc701a41b57d1
Xiaok <3795104+luxiaok@users.noreply.github.com>
2020-03-13 15:52:16 +0800

    Add shutdown status

--- handler/guest.py
+++ handler/guest.py
@@ -13,5 +13,5 @@ class IndexHandler(BaseHandler):
     def get(self):
         k = kvm()
         guests = k.getGuests()
-        status = ['',u'<span style="color:green;">运行中</span>']
+        status = [u'<span style="color:#ccc;">已关机</span>',u'<span style="color:green;">运行中</span>']
         self.render('guest/index.html',guests=guests,status=status)
