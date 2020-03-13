355a7911674822ddc2fb1fbe8d9e457494888de2
Xiaok <3795104+luxiaok@users.noreply.github.com>
2020-03-13 21:41:05 +0800

    Add page title for guest

--- handler/guest.py
+++ handler/guest.py
@@ -14,12 +14,14 @@ class IndexHandler(BaseHandler):
         k = kvm()
         guests = k.getGuests()
         status = [u'<span style="color:#ccc;">已关机</span>',u'<span style="color:green;">运行中</span>']
+        self.title = u'虚拟机实例'
         self.render('guest/index.html',guests=guests,status=status)
 
 
 class CreateGuestHandler(BaseHandler):
 
     def get(self):
+        self.title = u'创建虚拟机'
         self.render('guest/create.html')
 
 
@@ -27,5 +29,6 @@ class DetailHandler(BaseHandler):
 
     def get(self):
         name = self.get_argument('name')
+        self.title = u'虚拟机详情 - ' + name
         self.render('guest/detail.html',name=name)
 
