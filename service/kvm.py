1cf6304f5dd35803a5c9c8b5170e2b41a874f4cb
Xiaok <3795104+luxiaok@users.noreply.github.com>
2020-03-13 15:46:48 +0800

    getHdd: improve algo for disk

--- service/kvm.py
+++ service/kvm.py
@@ -25,7 +25,7 @@ class kvm:
     # disks = xml.getElementsByTagName('disk')
     def getHdd(self,disks):
         hdd = []
-        type_to_src = {'file': 'file', 'block': 'dev'}
+        type_to_path = {'file': 'file', 'block': 'dev'}
         if len(disks) > 0:
             for d in disks:
                 if d.getAttribute('device') == 'disk':  # disk or cdrom
@@ -33,9 +33,13 @@ class kvm:
                     hddNodes = d.childNodes
                     for n in hddNodes:
                         if n.nodeName == 'source':
-                            hdd.append(n.getAttribute(type_to_src[hdd_type]))  # hdd src, eg: /kvm/images/guest.img
+                            hdd_path = n.getAttribute(type_to_path[hdd_type]) # hdd src, eg: /kvm/images/guest.img
+                            vol = self.conn.storageVolLookupByPath(hdd_path)
+                            info = vol.info()
+                            total = info[1]/1024.0/1024/1024
+                            used = info[2]/1024.0/1024/1024
+                            hdd.append("%.2f G / %.2f G" % (used,total))
         return hdd
-
 
 
     def getGuests(self):
