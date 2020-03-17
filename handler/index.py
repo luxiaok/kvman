a995e019ced691c3183b82d4118c88567a16aeff
Xiaok <3795104+luxiaok@users.noreply.github.com>
2020-03-17 10:45:44 +0800

    Add version info to index page

--- handler/index.py
+++ handler/index.py
@@ -5,11 +5,15 @@
 
 from BaseHandler import BaseHandler
 #from tornado.web import authenticated as Auth
+from service.kvm import kvm
 
 class IndexHandler(BaseHandler):
 
     #@Auth
     def get(self):
         #self.log.info('Hello,Index page!') # Log Test
+        k = kvm()
+        version = k.getVersion()
+        version['app'] = self.app_version
         self.title = u'控制台'
-        self.render('index/index.html',version=self.app_version)
+        self.render('index/index.html',version=version)
