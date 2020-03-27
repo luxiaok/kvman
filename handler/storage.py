62232b9b3c99a3e62784379404a709a18c6e981f
Xiaok <3795104+luxiaok@users.noreply.github.com>
2020-03-27 14:15:58 +0800

    Add getStoragePools to storage handler

--- handler/storage.py
+++ handler/storage.py
@@ -5,11 +5,14 @@
 
 from BaseHandler import BaseHandler
 #from tornado.web import authenticated as Auth
+from service.kvm import kvm
 
 class IndexHandler(BaseHandler):
 
     #@Auth
     def get(self):
         #self.log.info('Hello,Index page!') # Log Test
+        k = kvm()
+        storages = k.getStoragePools()
         self.title = u'存储'
         self.render('storage/index.html')
