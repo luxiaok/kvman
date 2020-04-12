69d745f1fa48b957190678ec5d43581f70bb159e
Xiaok <3795104+luxiaok@users.noreply.github.com>
2020-04-12 17:44:52 +0800

    Add server py

---
+++ handler/server.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+# Powered By KK Studio
+# Server Page
+
+from BaseHandler import BaseHandler
+from tornado.web import authenticated as Auth
+#from service.kvm import kvm
+
+
+class IndexHandler(BaseHandler):
+
+    @Auth
+    def get(self):
+        #k = kvm()
+        self.title = u'服务器'
+        self.render('server/index.html')
