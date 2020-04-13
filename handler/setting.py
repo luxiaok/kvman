aefe7b15a5b81dd23b81e04bc80938c9cf6985cb
Xiaok <3795104+luxiaok@users.noreply.github.com>
2020-04-13 15:41:56 +0800

    Add setting data

--- handler/setting.py
+++ handler/setting.py
@@ -12,3 +12,13 @@ class IndexHandler(BaseHandler):
     def get(self):
         self.title = u'系统设置'
         self.render('setting/index.html')
+
+
+    def post(self):
+        username = self.get_argument('username')
+        password = self.get_argument('password')
+        protocol = self.get_argument('protocol')
+        hostname = self.get_argument('hostname')
+        qemu_username = self.get_argument('qemu_username')
+        qemu_password = self.get_argument('qemu_password')
+
