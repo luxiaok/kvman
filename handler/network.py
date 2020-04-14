275d79d3fc987109cad5534c8a786d004148b15d
Xiaok <3795104+luxiaok@users.noreply.github.com>
2020-04-14 10:22:56 +0800

    Add auth decorate for network handler

--- handler/network.py
+++ handler/network.py
@@ -2,13 +2,12 @@
 # -*- coding:utf-8 -*-
 # Powered By KK Studio
 
-
 from BaseHandler import BaseHandler
-#from tornado.web import authenticated as Auth
+from tornado.web import authenticated as Auth
 
 class IndexHandler(BaseHandler):
 
-    #@Auth
+    @Auth
     def get(self):
         self.title = u'网络'
         self.render('network/index.html')
