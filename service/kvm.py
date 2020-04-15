5495dc8b74a0751b1cbf93b745d097dc62cebd50
Xiaok <3795104+luxiaok@users.noreply.github.com>
2020-04-15 18:03:25 +0800

    Sort by name for guests

--- service/kvm.py
+++ service/kvm.py
@@ -147,7 +147,7 @@ class kvm:
                 'status': i.isActive()
             }
             guests.append(dom)
-        return sorted(guests)
+        return sorted(guests,key=lambda guest : guest['name']) # Sort by Name
 
 
     def startGuest(self,name):
