57fbfbb2e6f77c5cf0f858c2d78daee203ce50db
Xiaok <3795104+luxiaok@users.noreply.github.com>
2020-04-18 10:09:20 +0800

    Add kvman.random method

--- static/js/kvman.js
+++ static/js/kvman.js
@@ -13,4 +13,14 @@ $(function () {
     kvman = {
         foo: 'bar'
     };
+
+
+    //生成随机字符串
+    kvman.random = function(len){
+        if (!len) len = 8; //默认长度
+        var _org_num = Math.random();
+        if (_org_num<0.1) len++; //解决0.0xxxx导致生成随机数位数不足问题
+        var len_num = Math.pow(10,len);
+        return parseInt(_org_num*len_num);
+    };
 });
