ff37b2b9c99d31b09c82fed18c5ff77d06dc4269
Xiaok <3795104+luxiaok@users.noreply.github.com>
2020-04-18 10:15:28 +0800

    Add kvman.log method

--- static/js/kvman.js
+++ static/js/kvman.js
@@ -9,9 +9,35 @@ $(function () {
     //绑定Tooltip
     $('[data-toggle="tooltip"]').tooltip();
 
+
     //定义Kvman全局变量
     kvman = {
         foo: 'bar'
+    };
+
+
+    //获取当前时间，返回格式：yyyy-mm-dd hh:mm:ss
+    kvman.get_time = function(){
+        var _date = new Date(),
+            year = _date.getFullYear(),
+            month = _date.getMonth() + 1, //注意：getMonth返回的数据是0-11
+            day = _date.getDate(),
+            hours = _date.getHours(),
+            minutes = _date.getMinutes(),
+            seconds = _date.getSeconds();
+        month = month < 10 ? '0' + month : month;
+        day = day < 10 ? '0' + day : day;
+        hours = hours < 10 ? '0' + hours : hours;
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+        return year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + seconds;
+    };
+
+
+    //控制台带时间的日志输出方法
+    kvman.log = function(logs){
+        var now = '[' + kvman.get_time() + ']';
+        console.log(now,logs);
     };
 
 
