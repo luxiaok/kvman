c6ad668e2cc04c232813d65f7fce7f292a883f1b
Xiaok <3795104+luxiaok@users.noreply.github.com>
2020-04-22 17:13:34 +0800

    Handler: add edit kvm server api

--- handler/server.py
+++ handler/server.py
@@ -49,3 +49,36 @@ class CreateHandler(BaseHandler):
             return self.returnJson({'code': 0, 'msg': u'成功添加了一台 KVM 服务器！'})
         else:
             return self.returnJson({'code': -1, 'msg': u'添加失败！'})
+
+
+# 编辑KVM主机信息
+class EditHandler(BaseHandler):
+
+    @Auth
+    def post(self):
+        hostname0 = self.get_argument('hostname0') # 原始主机名
+        hostname = self.get_argument('hostname') # 新主机名
+        port = self.get_argument('port')
+        protocol = self.get_argument('protocol')
+        username = self.get_argument('username')
+        password = self.get_argument('password')
+        comments = self.get_argument('comments')
+        if not hostname or not protocol:
+            return self.returnJson({'code': -1, 'msg': u'请填写主机名和协议！'})
+        if hostname == hostname0: # 未改变主机名
+            host = self.get_kvm_server(hostname0) # 读取主机信息
+        else: # 修改了主机名
+            host = self.get_kvm_server(hostname) # 检测新主机名是否重复
+            if host:
+                return self.returnJson({'code': -1, 'msg': u'主机名重复！'})
+            host = {'hostname': hostname}
+        host['port'] = port
+        host['protocol'] = protocol
+        host['username'] = username
+        host['password'] = password
+        host['comments'] = comments
+        result = self.redis.hset(self.application.settings['kvm_servers_key'], hostname, json.dumps(host))
+        if result:
+            return self.returnJson({'code': 0, 'msg': u'保存成功！'})
+        else:
+            return self.returnJson({'code': -1, 'msg': u'保存失败！'})
