acadd6f04d18e42e75c839c18162d7269e7d6084
Xiaok <3795104+luxiaok@users.noreply.github.com>
2020-04-25 14:59:05 +0800

    Static: add route.GuestDetail all methods

--- static/js/kvman-lib.js
+++ static/js/kvman-lib.js
@@ -8,6 +8,17 @@
 //    console.log('Call default');
 //}
 
+
+/* Icon图标
+ * 0 - 叹号
+ * 1 - 打勾
+ * 2 - 打叉
+ * 3 - 问号
+ * 4 - 锁
+ * 5 - 难过
+ * 6 - Smile
+ * */
+
 /******** Base Module ********/
 
 export const k = {
@@ -45,7 +56,7 @@ export const k = {
 const guest_start = function (name, status) {
     var msg = '您确定要将虚拟机 ' + name + ' 开机吗？';
     if (status === 1) {
-        layer.alert('该虚拟机已是开机状态！', {title: '开机提示', icon: 1});
+        layer.alert('该虚拟机已是开机状态！', {title: '开机提示', icon: 0});
         return false;
     }
     layer.confirm(msg, {icon: 3, title: '开机提示'}, function (index) {
@@ -66,12 +77,12 @@ const guest_start = function (name, status) {
                     layer.msg(msg);
                 } else {
                     layer.close(index);
-                    layer.alert('开机失败，请稍后再试！', {title: '开机提示', icon: 3});
+                    layer.alert('开机失败，请稍后再试！', {title: '开机提示', icon: 0});
                 }
             },
             error: function () {
                 layer.close(index);
-                layer.alert('系统繁忙，请稍后再试！', {title: '开机提示', icon: 3});
+                layer.alert('系统繁忙，请稍后再试！', {title: '开机提示', icon: 2});
             }
         });
     });
@@ -82,7 +93,7 @@ const guest_start = function (name, status) {
 const guest_shutdown = function (name, status) {
     var msg = '您确定要将虚拟机 ' + name + ' 关机吗？';
     if (status === 0) {
-        layer.alert('该虚拟机已是关机状态！', {title: '关机提示', icon: 1});
+        layer.alert('该虚拟机已是关机状态！', {title: '关机提示', icon: 0});
         return false;
     }
     layer.confirm(msg, {icon: 3, title: '关机提示'}, function (index) {
@@ -103,12 +114,12 @@ const guest_shutdown = function (name, status) {
                     layer.msg(msg);
                 } else {
                     layer.close(index);
-                    layer.alert('关机失败，请稍后再试！', {title: '关机提示', icon: 3});
+                    layer.alert('关机失败，请稍后再试！', {title: '关机提示', icon: 0});
                 }
             },
             error: function () {
                 layer.close(index);
-                layer.alert('系统繁忙，请稍后再试！', {title: '关机提示', icon: 3});
+                layer.alert('系统繁忙，请稍后再试！', {title: '关机提示', icon: 2});
             }
         });
     });
@@ -119,7 +130,7 @@ const guest_shutdown = function (name, status) {
 const guest_reboot = function (name, status) {
     var msg = '您确定要将虚拟机 ' + name + ' 重启吗？';
     if (status === 0) {
-        layer.alert('该虚拟机暂未开机！', {title: '重启提示', icon: 1});
+        layer.alert('该虚拟机暂未开机！', {title: '重启提示', icon: 0});
         return false;
     }
     layer.confirm(msg, {icon: 3, title: '重启提示'}, function (index) {
@@ -127,6 +138,7 @@ const guest_reboot = function (name, status) {
         layer.close(index);
     });
 };
+
 
 /* 自动启动 */
 const guest_autostart = function (name, autostart) {
@@ -156,12 +168,12 @@ const guest_autostart = function (name, autostart) {
                     layer.msg(msg);
                 } else {
                     layer.close(index);
-                    layer.alert('设置失败，请稍后再试！', {title: 'KvMan提示', icon: 3});
+                    layer.alert('设置失败，请稍后再试！', {title: 'KvMan提示', icon: 0});
                 }
             },
             error: function () {
                 layer.close(index);
-                layer.alert('系统繁忙，请稍后再试！', {title: 'KvMan提示', icon: 3});
+                layer.alert('系统繁忙，请稍后再试！', {title: 'KvMan提示', icon: 2});
             }
         });
     });
@@ -188,12 +200,26 @@ const guest_console = function (name, status) {
             } else if (code < 0) {
                 layer.msg(msg);
             } else {
-                layer.alert('获取远程连接失败，请稍后再试！', {title: 'KvMan提示', icon: 3});
+                layer.alert('获取远程连接失败，请稍后再试！', {title: 'KvMan提示', icon: 0});
             }
         },
         error: function () {
-            layer.alert('系统繁忙，请稍后再试！', {title: 'KvMan提示', icon: 3});
+            layer.alert('系统繁忙，请稍后再试！', {title: 'KvMan提示', icon: 2});
         }
+    });
+};
+
+
+/* 销毁虚拟机 */
+const guest_destroy = function (name, status) {
+    var msg = '您确定要将虚拟机 ' + name + ' 彻底销毁吗？';
+    if (status === 1) {
+        layer.alert('请先将虚拟机关闭！', {title: '销毁提示', icon: 0});
+        return false;
+    }
+    layer.confirm(msg, {icon: 3, title: '销毁提示'}, function (index) {
+        layer.msg('正在销毁……');
+        layer.close(index);
     });
 };
 
@@ -236,7 +262,6 @@ route.Guest = {
             guest_reboot(name,status);
         });
 
-
         //自动启动
         $('.autostart-btn').click(function () {
             var name = $(this).data('name'),
@@ -244,12 +269,52 @@ route.Guest = {
             guest_autostart(name,autostart);
         });
 
-
         //远程连接
         $('.console-btn').click(function () {
             var name = $(this).data('name'),
                 status = $(this).data('status');
             guest_console(name,status);
+        });
+    }
+};
+
+
+route.GuestDetail = {
+    uri: '/guest/detail',
+    init: function () {
+        //开机
+        $('#start-btn').click(function () {
+            var name = $('#name').val().trim(),
+                status = $('#name').data('status');
+            guest_start(name,status);
+        });
+
+        //关机
+        $('#halt-btn').click(function () {
+            var name = $('#name').val().trim(),
+                status = $('#name').data('status');
+            guest_shutdown(name,status);
+        });
+
+        //重启
+        $('#reboot-btn').click(function () {
+            var name = $('#name').val().trim(),
+                status = $('#name').data('status');
+            guest_reboot(name,status);
+        });
+
+        //远程连接
+        $('#console-btn').click(function () {
+            var name = $('#name').val().trim(),
+                status = $('#name').data('status');
+            guest_console(name,status);
+        });
+
+        //销毁虚拟机
+        $('#destroy-btn').click(function () {
+            var name = $('#name').val().trim(),
+                status = $('#name').data('status');
+            guest_destroy(name,status);
         });
     }
 };
