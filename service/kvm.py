c28d571548770b646bd9d7090ad5b961595bf0bd
Xiaok <3795104+luxiaok@users.noreply.github.com>
2020-05-03 15:56:58 +0800

    Kvm: add os_type lists

--- service/kvm.py
+++ service/kvm.py
@@ -13,6 +13,46 @@ from xml.dom import minidom
 class kvm:
 
 
+    os_type = {
+        'Linux': [
+            {'os_id': 'centos5.11', 'name': 'CentOS 5'},
+            {'os_id': 'centos6.10', 'name': 'CentOS 6'},
+            {'os_id': 'centos7.0', 'name': 'CentOS 7'},
+            {'os_id': 'rhel8.0', 'name': 'CentOS 8'},
+            {'os_id': 'rhel5.11', 'name': 'RHEL 5'},
+            {'os_id': 'rhel6.10', 'name': 'RHEL 6'},
+            {'os_id': 'rhel7.8', 'name': 'RHEL 7'},
+            {'os_id': 'rhel8.0', 'name': 'RHEL 8'},
+            {'os_id': 'ubuntu18.04', 'name': 'Ubuntu'},
+            {'os_id': 'fedora29', 'name': 'Fedora'},
+            {'os_id': 'ubuntu18.04', 'name': 'OpenWrt'},
+            {'os_id': 'centos6.10', 'name': 'Other Linux kernel OS'},
+        ],
+        'Windows': [
+            {'os_id': 'win2k3', 'name': 'Windows Server 2003'},
+            {'os_id': 'win2k8', 'name': 'Windows Server 2008'},
+            {'os_id': 'winxp', 'name': 'Windows XP'},
+            {'os_id': 'win7', 'name': 'Windows 7'},
+            {'os_id': 'win8', 'name': 'Windows 8'},
+            {'os_id': 'win10', 'name': 'Windows 10'},
+        ],
+        'Android': [
+            {'os_id': '', 'name': 'Android x86 4.0'},
+            {'os_id': '', 'name': 'Android x86 5.0'},
+            {'os_id': '', 'name': 'Android x86 6.0'},
+            {'os_id': '', 'name': 'Android x86 7.0'},
+            {'os_id': '', 'name': 'Android x86 8.0'},
+            {'os_id': '', 'name': 'Android x86 9.0'},
+        ],
+        'MacOS': [
+            {'os_id': 'macosx10.7', 'name': 'macOS Sierra 10.12.6'},
+            {'os_id': 'macosx10.7', 'name': 'macOS High Sierra 10.13.6'},
+            {'os_id': 'macosx10.7', 'name': 'macOS Mojave 10.14.6'},
+            {'os_id': 'macosx10.7', 'name': 'macOS Catalina 10.15.4'},
+        ]
+    }
+
+
     def __init__(self,uri=None):
         #print 'Init kvm service ...'
         self.code = 0
