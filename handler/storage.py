9041357b5963247da1d4388e8d69b139b27601c4
Xiaok <3795104+luxiaok@users.noreply.github.com>
2020-05-13 10:16:00 +0800

    Storage: delete storage_type

--- handler/storage.py
+++ handler/storage.py
@@ -7,21 +7,11 @@ from tornado.web import authenticated as Auth
 import libvirt
 
 
-storage_type = {
-    libvirt.VIR_CONNECT_LIST_STORAGE_POOLS_DIR: u'目录',
-    libvirt.VIR_CONNECT_LIST_STORAGE_POOLS_LOGICAL: u'逻辑卷',
-    libvirt.VIR_CONNECT_LIST_STORAGE_POOLS_NETFS: u'NFS',
-    libvirt.VIR_CONNECT_LIST_STORAGE_POOLS_DISK: u'磁盘',
-    libvirt.VIR_CONNECT_LIST_STORAGE_POOLS_FS: u'文件系统',
-    libvirt.VIR_CONNECT_LIST_STORAGE_POOLS_SCSI: u'SCSI',
-    libvirt.VIR_CONNECT_LIST_STORAGE_POOLS_ISCSI: u'iSCSI'
-}
-
-
 class IndexHandler(BaseHandler):
 
     @Auth
     def get(self):
+        print storage_type
         storages = self.kvm.getStoragePools()
         self.render('storage/index.html',storages=storages)
 
