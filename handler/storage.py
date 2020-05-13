eabb2b71c424c2ff131fe02f2bcb7d59373fa675
Xiaok <3795104+luxiaok@users.noreply.github.com>
2020-05-13 10:06:53 +0800

    Storage: add storage_type

--- handler/storage.py
+++ handler/storage.py
@@ -4,6 +4,18 @@
 
 from BaseHandler import BaseHandler
 from tornado.web import authenticated as Auth
+import libvirt
+
+
+storage_type = {
+    libvirt.VIR_CONNECT_LIST_STORAGE_POOLS_DIR: u'目录',
+    libvirt.VIR_CONNECT_LIST_STORAGE_POOLS_LOGICAL: u'逻辑卷',
+    libvirt.VIR_CONNECT_LIST_STORAGE_POOLS_NETFS: u'NFS',
+    libvirt.VIR_CONNECT_LIST_STORAGE_POOLS_DISK: u'磁盘',
+    libvirt.VIR_CONNECT_LIST_STORAGE_POOLS_FS: u'文件系统',
+    libvirt.VIR_CONNECT_LIST_STORAGE_POOLS_SCSI: u'SCSI',
+    libvirt.VIR_CONNECT_LIST_STORAGE_POOLS_ISCSI: u'iSCSI'
+}
 
 
 class IndexHandler(BaseHandler):
