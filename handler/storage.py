26f088e8446c500d479e46ea31d71961cf999edc
Xiaok <3795104+luxiaok@users.noreply.github.com>
2020-05-18 20:41:20 +0800

    Storage: add RefreshStorageHandler

--- handler/storage.py
+++ handler/storage.py
@@ -22,3 +22,12 @@ class VolumeHandler(BaseHandler):
         vols = self.kvm.getStorageVols(pool)
         self.render('storage/volume.html',vols=vols,pool=pool)
 
+
+# 刷新存储池
+class RefreshStorageHandler(BaseHandler):
+
+    @Auth
+    def post(self):
+        pool = self.get_argument('pool')
+        self.kvm.refreshStorage(pool)
+        self.returnJson({'code': 0, 'msg': u'刷新成功！'})
